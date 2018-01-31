9ebc2c98287390bd3d554a4129b0e27620cc25a5
May Jawdat <may.jawdat@gmail.com>
2018-01-31 14:47:37 -0800

    Update User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  
+
   has_many :messages
   has_many :locations
 
   has_secure_password
 
-  validates :username, :first_name, :last_name, :phone_number, presence: true
-  validates :username, uniqueness: true
+  validates :username, :email, :first_name, :last_name, :phone_number, presence: true
+  validates :username, :email, uniqueness: true
 
 end
